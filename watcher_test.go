be08ea38970231184844b58a75acbeba367cdb66
Benny Ng <benny.tpng@gmail.com>
2014-10-21 11:30:30 +0800

    add test cases for template register ordering

--- watcher_test.go
+++ watcher_test.go
@@ -12,7 +12,27 @@ const (
 	globKey
 )
 
-func TestFiles(t *testing.T) {
+func TestBaseFilesThenFiles(t *testing.T) {
+	if err := RegisterBaseFiles("base.html"); err != nil {
+		t.Error(err)
+	}
+	if err := RegisterFiles(filesKey, "test.html"); err != nil {
+		t.Error(err)
+	}
+	temp, err := Get(filesKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b := new(bytes.Buffer)
+	if err := temp.ExecuteTemplate(b, "base.html", nil); err != nil {
+		t.Error(err)
+	}
+	if bytes.Compare(b.Bytes(), []byte("Base Test")) != 0 {
+		t.Fatalf("expected %q, got %q", "Base Test", b.String())
+	}
+}
+
+func TestFilesThenBaseFiles(t *testing.T) {
 	if err := RegisterFiles(filesKey, "test.html"); err != nil {
 		t.Error(err)
 	}
@@ -32,11 +52,31 @@ func TestFiles(t *testing.T) {
 	}
 }
 
-func TestGlob(t *testing.T) {
+func TestBaseGlobThenGlob(t *testing.T) {
 	if err := RegisterBaseGlob("base.html"); err != nil {
 		t.Error(err)
 	}
 	if err := RegisterGlob(globKey, "test.html"); err != nil {
+		t.Error(err)
+	}
+	temp, err := Get(globKey)
+	if err != nil {
+		t.Error(err)
+	}
+	b := new(bytes.Buffer)
+	if err := temp.Execute(b, nil); err != nil {
+		t.Error(err)
+	}
+	if bytes.Compare(b.Bytes(), []byte("Base Test")) != 0 {
+		t.Fatalf("expected %q, got %q", "Base Test", b.String())
+	}
+}
+
+func TestGlobThenBaseGlob(t *testing.T) {
+	if err := RegisterGlob(globKey, "test.html"); err != nil {
+		t.Error(err)
+	}
+	if err := RegisterBaseGlob("base.html"); err != nil {
 		t.Error(err)
 	}
 	temp, err := Get(globKey)
